8c39923c921dfadb4daa08bc560f185340737acc
kustex <lucaskustermans@gmail.com>
2020-11-13 20:55:02 +0100

    copy of local .py file

--- model_3d_house_project.py
+++ model_3d_house_project.py
@@ -9,30 +9,36 @@ from rasterio.windows import from_bounds
 from rasterio.enums import Resampling
 from rasterio.plot import show
 from pyproj import Proj, transform
+import plotly.graph_objects as go
 
 img = rasterio.open('data/geotif/dsm/DHMVIIDSMRAS1m_k13.tif')
 
-#ask for coordinates
-long_coordinate = float(input("What is the longitude of your coordinate?"))
-lat_coordinate = float(input("What is the latidude of your coordinate?"))
+#convert DMS to DD
+degrees_coordinate_lat = float(input("What are the degrees of your latitude coordinate?"))
+minutes_coordinate_lat= float(input("What are the minutes of your latitude coordinate?"))
+seconds_coordinate_lat = float(input("What are the seconds of your latitude coordinate?")) 
+dd_lat = degrees_coordinate_lat + (minutes_coordinate_lat/60) + (seconds_coordinate_lat/3600)
 
-#print("%0.10f %0.10f" % (long_coordinate, lat_coordinate))
+degrees_coordinate_long = float(input("What are the degrees of your longitude coordinate?"))
+minutes_coordinate_long = float(input("What are the minutes of your longitude coordinate?"))
+seconds_coordinate_long = float(input("What are the seconds of your longitude coordinate?")) 
+dd_long = degrees_coordinate_long + (minutes_coordinate_long/60) + (seconds_coordinate_long/3600)
 
-long_bruge = 3.22424
-lat_bruge = 51.20892
+print(dd_lat, dd_long)
 
-#print("%0.10f %0.10f" % (long_bruge, lat_bruge))
+#long_bruge = 3.22424
+#lat_bruge = 51.20892
 
 #transform from long, lat (degrees) to x, y coordinates
 epsg31370 = Proj(init='epsg:31370')
-x, y = epsg31370(long_coordinate, lat_coordinate)
-print(x, y)
+x, y = epsg31370(dd_lat, dd_long)
+print(y, x)
 
 #create bounding box around x, y coordinates
-x_left = x - 10
-x_right = x + 10
-y_top = y + 10
-y_bottom = y - 10
+x_left = x - 50
+x_right = x + 50
+y_top = y + 50
+y_bottom = y - 50
 
 #read and plot the GeoTiff file
 rst = img.read(1, window=from_bounds(x_left, y_bottom, x_right, y_top, img.transform))
@@ -42,12 +48,16 @@ rst = img.read(1, window=from_bounds(x_left, y_bottom, x_right, y_top, img.trans
 rst_gdf = gpd.GeoDataFrame(rst)
 
 #making surfaceplot out of geodataframe
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+#fig = plt.figure()
+#ax = fig.gca(projection='3d')
+#
+#x = np.arange(0,80,1)
+#y = np.arange(0,80,1)
+#x, y = np.meshgrid(x,y)
+#
+#ax.plot_surface(x,y,rst_gdf, cmap=cm.coolwarm)
+#plt.show()
 
-x = np.arange(0,20,1)
-y = np.arange(0,20,1)
-x, y = np.meshgrid(x,y)
-
-surf = ax.plot_surface(x,y,rst_gdf, cmap=cm.coolwarm)
-plt.show()
+fig = go.Figure(data=[go.Surface(z=rst_gdf.values)])
+fig.update_layout(title='Surface plot', autosize=True)
+fig.show()
